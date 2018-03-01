0766bd37dec07b10b3604550cf6921432fd21aee
RyanLMurphy <rmurphy2718@gmail.com>
2018-03-01 10:54:47 -0500

    Added first file

---
+++ GenieImpl1.R
@@ -0,0 +1,5 @@
+############################################################
+# GenieImpl1.R
+# Ryan Murphy
+# Quck and dirty implementation of adaptive path layer
+############################################################
